b8122265cc7d949108628929aeb65989d95968d4
Max2002 <makss.liashik@gmail.com>
2022-09-30 16:25:59 +0300

    delete comments in api.js and fixed file .gitignore

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,8 @@
 import axios from 'axios';
 
 const BASE_API_URL = 'https://randomuser.me/api/';
-// const API_KEY = 'cddb56e1c82f5dfd7b5c73aad7df5f84';
 const instance = axios.create({
   baseURL: BASE_API_URL,
 });
 
-// instance.interceptors.request.use((config) => {
-//   // eslint-disable-next-line no-param-reassign
-//   config.params.appid = API_KEY;
-//
-//   return config;
-// });
 export default instance;
