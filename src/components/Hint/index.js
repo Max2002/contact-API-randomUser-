dc554b9d82608e2572b0876227ed9afd9a84edf8
Max2002 <makss.liashik@gmail.com>
2022-10-21 11:15:54 +0300

    Removed hover effect hint from copyElement to component hint.

--- src/components/Hint/index.js
+++ src/components/Hint/index.js
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
 import st from './styles.module.scss';
 
-export default function Hint({ children, className }) {
-  return <div className={clsx(st.hint, className)}>{children}</div>;
+export default function Hint({ children, label }) {
+  return (
+    <div className={st.hoverElement}>
+      <div className={st.hint}>{label}</div>
+      <span>{children}</span>
+    </div>
+  );
 }
 
-Hint.defaultProps = {
-  className: '',
-};
-
 Hint.propTypes = {
-  children: PropTypes.string.isRequired,
-  className: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
